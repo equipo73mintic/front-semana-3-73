32f7958ee0a6dccbfacc770efb086d55780e58a9
= <gclaros@unicauca.edu.co>
2020-12-19 08:49:26 -0500

    Change Home

--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -1,78 +1,67 @@
 <template>
-<div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">
-            <h4>HOME</h4>
-        </a>
-          
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-            
-            </ul>
-    
-            <div>
-                <button 
-                    class="btn btn-secondary " 
-                    type="button" 
-                    id="signOutButton" 
-                    @click="logOut">
-                    Log Out
-                </button>
-            
-            </div>
-        
+  <div>
+    <nav class="navbar navbar-expand-lg" style="background-color: #4163c7">
+      <a class="navbar-brand" >
+        <span style="color: #FFFFFF">HOME</span>
+      </a>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto"></ul>
+
+        <div>
+          <button
+            class="btn btn-secondary"
+            type="button"
+            id="signOutButton"
+            style="background-color: #ece3e1"
+            @click="logOut"
+          >
+            <span style="color: #4163c7">Logout</span>
+          </button>
         </div>
+      </div>
     </nav>
 
-  <div class="container mt-3 col-md-5">
+    <div class="container mt-3 col-md-5">
       <div class="card card-body">
-        <h3>
-            Datos de usuario
-        </h3>
-        <hr>
-        <p>
-            Nombre: {{ user.name }}
-        </p>
-        <p>
-            Email: {{ user.email }}
-        </p>
+        <h3>Bienvenido Usuario</h3>
+        <hr />
+
+        <p>Nombre: {{ user.name }}</p>
+        <p>Email: {{ user.email }}</p>
       </div>
-
-
+    </div>
   </div>
-  
-</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import VueJwtDecode from 'vue-jwt-decode';
+import VueJwtDecode from "vue-jwt-decode";
 
 export default {
-  data(){
-    return{
-        user: {}
-    }
+  data() {
+    return {
+      user: {},
+    };
   },
   methods: {
-    getUserDetails(){
+    getUserDetails() {
+      let token = localStorage.getItem("jwt");
+      let user = VueJwtDecode.decode(token);
 
-        let token = localStorage.getItem('jwt');
-        let user = VueJwtDecode.decode(token);
-
-        if (token){
-            this.user = user;
-        }
+      if (token) {
+        this.user = user;
+      }
     },
-    logOut(){
-        localStorage.removeItem('jwt');
-        // localStorage.removeItem('user');
+    logOut() {
+      localStorage.removeItem("jwt");
+      // localStorage.removeItem('user');
 
-        this.$router.push('/');
-    }
+      this.$router.push("/");
+    },
   },
-  created(){
-      this.getUserDetails();
-  }
-}
+  created() {
+    this.getUserDetails();
+  },
+};
 </script>
